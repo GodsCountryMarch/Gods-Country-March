d880587571ded00532126b015734dc3825b428ac
GodsCountryMarch <godscountrymarch@gmail.com>
2025-09-16 20:19:36 -0600

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Using the built-in Vite React plugin via dev dependency in vite (no extra config needed)
-export default defineConfig({
-  plugins: [react()],
-})
